1b9031884aa890768c1b3c09f3010d62cab8fa7f
cesarapll <cpizarrollanos@gmail.com>
2023-03-22 01:40:57 -0500

    chore: Container with status bar height as margin

--- mood-app/App.tsx
+++ mood-app/App.tsx
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Constants from 'expo-constants';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>App</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -12,9 +13,7 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: Constants.statusBarHeight,
+    flexGrow: 1,
   },
 });
